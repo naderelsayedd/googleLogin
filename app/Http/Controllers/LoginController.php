048a35d242d9978c53a7a8a80d6871d0ea21695c
Nader El Sayed  Abdulhamid <elsayednder@gmail.com>
2024-09-27 19:54:44 +0300

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    // Redirect to Google
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    // Handle Google callback
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+            $user = User::firstOrCreate(
+                ['email' => $googleUser->getEmail()],
+                ['name' => $googleUser->getName(), 'password' => bcrypt('secret')]
+            );
+            Auth::login($user);
+            return redirect('/dashboard');
+        } catch (\Exception $e) {
+            return redirect('/login');
+        }
+    }
+}
